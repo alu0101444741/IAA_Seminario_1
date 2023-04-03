8f6a4285257677c648afa6e295aec38db21031bb
EV2872 <alu0101395548@ull.edu.es>
2023-04-03 15:13:22 +0100

    Aprendizaje añadido

--- BotShooterIAA.py
+++ BotShooterIAA.py
@@ -2,6 +2,7 @@ import pysmile
 import pysmile_license
 from colorama import init, Fore
 import random
+import os
 # Tutorial2 loads the XDSL file created by Tutorial1,
 # then performs the series of inference calls,
 # changing evidence each time.
@@ -12,49 +13,28 @@ class BotShooterIAA:
         net = pysmile.Network()
         
         # load the network created by Tutorial1
-        net.read_file("red_bot.xdsl")
+        net.read_file("red_bot_2.xdsl")
         
        # print(Fore.MAGENTA + "Tabla de la red bayesiana sin evidencias:")
         net.update_beliefs()
+
+        os.system('cls||clear')
+        
         print("")
         print(Fore.RED + "Tablas con los valores del fichero red_bot.xdsl" + Fore.WHITE)
         self.print_all_posteriors(net)
-        self.opcionRand = input("Fijar Evidencias: aleatorio? (" + Fore.RED + "Y" + Fore.WHITE + ")/(" + Fore.RED + "N" + Fore.WHITE + "): ")
-        if self.opcionRand == "Y" or self.opcionRand == "y":
-            print("Modo aleatorio activado")
-            print("")
-            self.si_no = ""
-            self.aleatorio = random.randint(0, 1)
-            self.si_no = "bajo" if self.aleatorio == 0 else "alto"
-            self.change_evidence_and_update(net, "H", self.si_no, False)
-
-            self.aleatorio = random.randint(0, 1)
-            self.si_no = "armado_no" if self.aleatorio == 0 else "armado_si"
-            self.change_evidence_and_update(net, "W", self.si_no, False)
-
-            self.aleatorio = random.randint(0, 1)
-            self.si_no = "armado_no" if self.aleatorio == 0 else "armado_si"
-            self.change_evidence_and_update(net, "OW", self.si_no, False)
-
-            self.aleatorio = random.randint(0, 1)
-            self.si_no = "oye_no" if self.aleatorio == 0 else "oye_si"
-            self.change_evidence_and_update(net, "HN", self.si_no, False)
-
-            self.aleatorio = random.randint(0, 1)
-            self.si_no = "cerca_no" if self.aleatorio == 0 else "cerca_si"
-            self.change_evidence_and_update(net, "NE", self.si_no, False)
-
-            self.aleatorio = random.randint(0, 1)
-            self.si_no = "cerca_no" if self.aleatorio == 0 else "cerca_si"
-            self.change_evidence_and_update(net, "PW", self.si_no, False)
-
-            self.aleatorio = random.randint(0, 1)
-            self.si_no = "cerca_no" if self.aleatorio == 0 else "cerca_si"
-            self.change_evidence_and_update(net, "PH", self.si_no, False)
-
-            print(Fore.RED + "Resultado con evidencias calculadas: ")
-            self.print_all_posteriors(net)
-            """
+        self.aprender = input("Desea añadir aprendizaje a la red (" + Fore.RED + "Y" + Fore.WHITE + ")/(" + Fore.RED + "N" + Fore.WHITE + "): ")
+        if self.aprender == "Y" or self.aprender == "y":
+            fichero = input("Introduzca el nombre del fichero: ")
+            # cargar los datos
+            ds = pysmile.learning.DataSet()
+            ds.read_file(fichero)
+            # emparejar datos y red
+            matching = ds.match_network(net)
+            # ejecutar el algoritmo de aprendizaje EM
+            em = pysmile.learning.EM()
+            em.learn(ds, net, matching)
+        """
             # cargar los datos
             ds = pysmile.learning.DataSet()
             ds.read_file("aprendizaje.txt")
@@ -66,57 +46,92 @@ class BotShooterIAA:
             em = pysmile.learning.EM()
             em.learn(ds, net, matching)
             """
+        self.opcionRand = input("Fijar Evidencias: aleatorio? (" + Fore.RED + "Y" + Fore.WHITE + ")/(" + Fore.RED + "N" + Fore.WHITE + "): ")
+        if self.opcionRand == "Y" or self.opcionRand == "y":
+            print("Modo aleatorio activado")
+            print("")
+            self.si_no = ""
+            self.aleatorio = random.randint(0, 1)
+            self.si_no = "Baja" if self.aleatorio == 0 else "Alta"
+            self.change_evidence_and_update(net, "H", self.si_no, False)
+
+            self.aleatorio = random.randint(0, 1)
+            self.si_no = "desarmado" if self.aleatorio == 0 else "armado"
+            self.change_evidence_and_update(net, "W", self.si_no, False)
+
+            self.aleatorio = random.randint(0, 1)
+            self.si_no = "desarmado" if self.aleatorio == 0 else "armado"
+            self.change_evidence_and_update(net, "OW", self.si_no, False)
+
+            self.aleatorio = random.randint(0, 1)
+            self.si_no = "no" if self.aleatorio == 0 else "si"
+            self.change_evidence_and_update(net, "HN", self.si_no, False)
+
+            self.aleatorio = random.randint(0, 1)
+            self.si_no = "no" if self.aleatorio == 0 else "si"
+            self.change_evidence_and_update(net, "NE", self.si_no, False)
+
+            self.aleatorio = random.randint(0, 1)
+            self.si_no = "no" if self.aleatorio == 0 else "si"
+            self.change_evidence_and_update(net, "PW", self.si_no, False)
+
+            self.aleatorio = random.randint(0, 1)
+            self.si_no = "no" if self.aleatorio == 0 else "si"
+            self.change_evidence_and_update(net, "PH", self.si_no, False)
+
+            print(Fore.RED + "Resultado con evidencias calculadas: ")
+            self.print_all_posteriors(net)
             
         elif self.opcionRand == "N" or self.opcionRand == "n": 
 
-            self.opcion = int(input("Evidencia Salud = bajo(" + Fore.RED + "0" + Fore.WHITE + ") / alto(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
+            self.opcion = int(input("Evidencia Salud = Baja(" + Fore.RED + "0" + Fore.WHITE + ") / Alta(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
             if (self.opcion == 0) :
-                self.change_evidence_and_update(net, "H", "bajo", False)
+                self.change_evidence_and_update(net, "H", "Baja", False)
             elif (self.opcion == 1) : 
-            #  print(Fore.MAGENTA + "Evidencia Salud = bajo.")
-                self.change_evidence_and_update(net, "H", "alto", False)
+            #  print(Fore.MAGENTA + "Evidencia Salud = Baja.")
+                self.change_evidence_and_update(net, "H", "Alta", False)
 
-            self.opcion = int(input("Evidencia Armas del bot en tiempo t = armado_no(" + Fore.RED + "0" + Fore.WHITE + ") / armado_si(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
+            self.opcion = int(input("Evidencia Armas del bot en tiempo t = desarmado(" + Fore.RED + "0" + Fore.WHITE + ") / armado(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
             if (self.opcion == 0) :
-                 self.change_evidence_and_update(net, "W", "armado_no", False)
+                 self.change_evidence_and_update(net, "W", "desarmado", False)
             elif (self.opcion == 1) :
-            #  print(Fore.MAGENTA + "Evidencia Armas del bot en tiempo t = armado_si.")
-                self.change_evidence_and_update(net, "W", "armado_si", False)
+            #  print(Fore.MAGENTA + "Evidencia Armas del bot en tiempo t = armado.")
+                self.change_evidence_and_update(net, "W", "armado", False)
 
-            self.opcion = int(input("Evidencia Oponente armado en tiempo t = armado_no(" + Fore.RED + "0" + Fore.WHITE + ") / armado_si(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
+            self.opcion = int(input("Evidencia Oponente armado en tiempo t = desarmado(" + Fore.RED + "0" + Fore.WHITE + ") / armado(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
             if (self.opcion == 0) :
-                self.change_evidence_and_update(net, "OW", "armado_no", False)
+                self.change_evidence_and_update(net, "OW", "desarmado", False)
             elif (self.opcion == 1) :
-            #  print(Fore.MAGENTA + "Evidencia Armas oponente en tiempo t = armado_si.")
-                self.change_evidence_and_update(net, "OW", "armado_si", False)
+            #  print(Fore.MAGENTA + "Evidencia Armas oponente en tiempo t = armado.")
+                self.change_evidence_and_update(net, "OW", "armado", False)
 
-            self.opcion = int(input("Evidencia Oye sonido en tiempo t = oye_no(" + Fore.RED + "0" + Fore.WHITE + ") / oye_si(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
+            self.opcion = int(input("Evidencia Oye sonido en tiempo t = no(" + Fore.RED + "0" + Fore.WHITE + ") / si(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
             if (self.opcion == 0) :
-                self.change_evidence_and_update(net, "HN", "oye_no", False)
+                self.change_evidence_and_update(net, "HN", "no", False)
             elif (self.opcion == 1) :
-            #  print(Fore.MAGENTA + "Evidencia Se oye sonido en tiempo t = oye_si.")
-                self.change_evidence_and_update(net, "HN", "oye_si", False)
+            #  print(Fore.MAGENTA + "Evidencia Se oye sonido en tiempo t = si.")
+                self.change_evidence_and_update(net, "HN", "si", False)
 
-            self.opcion = int(input("Evidencia Hay enemigos cercanos en tiempo t = cerca_no(" + Fore.RED + "0" + Fore.WHITE + ") / cerca_si(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
+            self.opcion = int(input("Evidencia Hay enemigos cercanos en tiempo t = no(" + Fore.RED + "0" + Fore.WHITE + ") / si(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
             if (self.opcion == 0) :
-                self.change_evidence_and_update(net, "NE", "cerca_no", False)
+                self.change_evidence_and_update(net, "NE", "no", False)
             elif (self.opcion == 1) :
-            #  print(Fore.MAGENTA + "Evidencia Numero de enemigos cercanos en tiempo t = cerca_si.")
-                self.change_evidence_and_update(net, "NE", "cerca_si", False)
+            #  print(Fore.MAGENTA + "Evidencia Numero de enemigos cercanos en tiempo t = si.")
+                self.change_evidence_and_update(net, "NE", "si", False)
 
-            self.opcion = int(input("Evidencia Hay una arma cerca en tiempo t = cerca_no(" + Fore.RED + "0" + Fore.WHITE + ") / cerca_si(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
+            self.opcion = int(input("Evidencia Hay una arma cerca en tiempo t = no(" + Fore.RED + "0" + Fore.WHITE + ") / si(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
             if (self.opcion == 0) :
-                self.change_evidence_and_update(net, "PW", "cerca_no", False)
+                self.change_evidence_and_update(net, "PW", "no", False)
             elif (self.opcion == 1) :
-            #  print(Fore.MAGENTA + "Evidencia hay un arma cercana en tiempo t = cerca_si.")
-                self.change_evidence_and_update(net, "PW", "cerca_si", False)
+            #  print(Fore.MAGENTA + "Evidencia hay un arma cercana en tiempo t = si.")
+                self.change_evidence_and_update(net, "PW", "si", False)
 
-            self.opcion = int(input("Evidencia Hay un paquete de salud cercano en tiempo t = cerca_no(" + Fore.RED + "0" + Fore.WHITE + ") / cerca_si(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
+            self.opcion = int(input("Evidencia Hay un paquete de salud cercano en tiempo t = no(" + Fore.RED + "0" + Fore.WHITE + ") / si(" + Fore.RED + "1" + Fore.WHITE + ") / saltar(" + Fore.RED + "2" + Fore.WHITE + "): "))
             if (self.opcion == 0) :
-                self.change_evidence_and_update(net, "PH", "cerca_no", False)
+                self.change_evidence_and_update(net, "PH", "no", False)
             elif (self.opcion == 1) :
-            #  print(Fore.MAGENTA + "Evidencia hay un paquete de salud cercano en el tiempo t = cerca_no.")
-                self.change_evidence_and_update(net, "PH", "cerca_si", False)
+            #  print(Fore.MAGENTA + "Evidencia hay un paquete de salud cercano en el tiempo t = no.")
+                self.change_evidence_and_update(net, "PH", "si", False)
 
             print("")
             print(Fore.RED + "Resultado con evidencias calculadas: ")
